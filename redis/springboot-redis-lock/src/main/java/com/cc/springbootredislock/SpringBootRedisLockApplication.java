4362d73478605ffdc2abb12574c1568ae67d0b09
carlosxiao <carlosxiao@dr.com>
2018-08-15 11:28:26 +0800

    Init Commit!

---
+++ redis/springboot-redis-lock/src/main/java/com/cc/springbootredislock/SpringBootRedisLockApplication.java
@@ -0,0 +1,12 @@
+package com.cc.springbootredislock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisLockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRedisLockApplication.class, args);
+	}
+}
